dd69ba2f3054901a544a4daab29c39ba840d6556
Quentin Garchery <garchery.quentin@gmail.com>
2018-04-11 17:25:32 +0200

    new pit proofs

--- temp.v
+++ temp.v
@@ -1,5 +1,23 @@
-Lemma bla A : A -> A.
+Lemma double_inv f :
+  (forall n, exists x, n = f (x))->
+  exists y, f (f (y)) = 3.
+
 
 Proof.
-  trivial.
+  intro.
+  pose (g := fun x : nat => 3).
+  destruct (H 3) as [x H3].
+  destruct (H x) as [x0 Hx].
+  rewrite Hx in H3.
+  symmetry in H3.
+  now exists x0.
 Qed.
+
+Print double_inv.
+
+Print list.
+
+
+
+
+
