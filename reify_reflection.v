098b5250eb1cc97ac81852fabb1a48221be29d1b
Quentin Garchery <garchery.quentin@gmail.com>
2018-08-01 16:26:50 +0200

    rapport 01/08

--- reify_reflection.v
+++ reify_reflection.v
@@ -21,13 +21,29 @@ Proof.
   | eapply EvalAnd ; [ apply EvalBool | apply EvalBool | reflexivity ]
   | reflexivity ].
 Qed.
-  
+
 Fixpoint evaluation (t : AndTree) :=
   match t with
     And t1 t2 => evaluation t1 && evaluation t2
-  | Bool n => n 
+  | Bool n => n
   end.
 
+
+Proposition Eval_eq_eval t b :
+  Evaluation t b <-> evaluation t = b.
+Proof.  
+  revert b. induction t as [t1 IHt1 t2 IHt2 | a]; simpl; intro b.
+  -split; intro H.
+   +inversion H.
+    apply IHt1 in H2; rewrite H2.
+    now apply IHt2 in H3; rewrite H3.
+   +eapply EvalAnd. now apply IHt1.
+    now apply IHt2. assumption.
+  -split; intro H.
+   +now inversion H.
+   +rewrite H. apply EvalBool.
+Qed.
+   
 Lemma eval_t_false : evaluation t = false.
 Proof.
   reflexivity.
@@ -48,14 +64,14 @@ Fixpoint peigne (t : AndTree) :=
   end.
 
 Inductive eqt : AndTree -> AndTree -> Prop :=
-| refl t : eqt t t
+  refl t : eqt t t
 | sym t1 t2 : eqt t1 t2 -> eqt t2 t1
 | assoc t1 t2 t3: eqt (And t1 (And t2 t3)) (And (And t1 t2) t3)
 | congru ta1 ta2 tb1 tb2 : eqt ta1 tb1 -> eqt ta2 tb2 ->
                            eqt (And ta1 ta2) (And tb1 tb2)
 | trans t1 t2 t3 : eqt t1 t2 -> eqt t2 t3 -> eqt t1 t3.
 
-Lemma eqt_is_eq t1 t2:
+Lemma eqt_correct t1 t2:
   eqt t1 t2 -> evaluation t1 = evaluation t2.
 Proof.
   intro eq12. induction eq12; simpl.
@@ -65,9 +81,9 @@ Proof.
   -rewrite IHeq12_1.
    now rewrite IHeq12_2.
   -now rewrite IHeq12_1.
-Qed.   
+Qed.
 
-Lemma append_correct t1 t2:
+Lemma append_eqt t1 t2:
   eqt (append t1 t2) (And t1 t2).
 Proof.
   revert t2. induction t1; intro t2; simpl.
@@ -76,16 +92,21 @@ Proof.
   -apply refl.
 Qed.
 
-Lemma peigne_correct t:
-  eqt t (peigne t).
-
+Lemma peigne_eqt t :
+  eqt (peigne t) t.
 Proof.
-  apply sym. induction t; simpl.
-  -eapply trans. apply append_correct. now apply congru.
+  induction t; simpl.
+  -eapply trans. apply append_eqt. now apply congru.
   -apply refl.
 Qed.
 
-Ltac reify A :=  match A with 
+Lemma peigne_correct t :
+  evaluation (peigne t) = evaluation t.
+Proof.
+  apply eqt_correct. now apply peigne_eqt.
+Qed.
+
+Ltac reify A :=  match A with
   | andb ?X ?Y => let rx := reify X in
                   let ry := reify Y in
                   constr:(And rx ry)
@@ -98,21 +119,13 @@ Ltac peignify :=
     let b := reify B in
     change A with (evaluation a);
     change B with (evaluation b);
-    rewrite (eqt_is_eq a _ (peigne_correct a));
-    rewrite (eqt_is_eq b _ (peigne_correct b));
+    rewrite <- (peigne_correct a);
+    rewrite <- (peigne_correct b);
     simpl
   end.
 
 Lemma peigne4 b1 b2 b3 b4:
   (b1 && b2) && (b3 && b4) = b1 && ((b2 && b3) && b4).
-
 Proof.
-  peignify. reflexivity.
-Qed.  
-
-Print peigne4.
-
-
-
-
-
+   peignify. reflexivity.
+Qed.
